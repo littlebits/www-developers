e34fcfa3f0b6cba72a7c529afa9de70fbf672cd3
Colin Vernon <colin@littlebits.cc>
2014-10-29 13:46:19 -0400

    new index endpoint

--- server/routes.js
+++ server/routes.js
@@ -1,82 +1,79 @@
-'use strict';
-var fs = require('fs');
-var path = require('path');
-var marked = require('marked');
+var fs = require('fs')
+var path = require('path')
+var marked = require('marked')
 
 function serve_file_md(file){
-  var md_content = fs.readFileSync(path.join(__dirname, '../views', file)).toString();
-  var html_content =  marked(md_content);
+  var md_content = fs.readFileSync(path.join(__dirname, '../views', file)).toString()
+  var html_content =  marked(md_content)
   return function(req, reply){
-    reply.view('index', {content: html_content});
-  };
+    reply.view('index', { content: html_content })
+  }
 }
 
 
 module.exports = function(server){
 
-  server.route({
-    path: '/api-http',
-    method: 'get',
-    handler: serve_file_md('endpoints.md')
-  });
+  server.route([
+    {
+      path: '/index',
+      method: 'get',
+      handler: serve_file_md('index.md')
+    },
+    {
+      path: '/',
+      method: 'get',
+      handler: function(request, reply) {
+        reply().redirect('/api-http');
+      }
+    },
+    {
+      path: '/api-http',
+      method: 'get',
+      handler: serve_file_md('endpoints.md')
+    },
+    {
+      path: '/api-http/auth',
+      method: 'get',
+      handler: serve_file_md('authorization.md')
+    },
+    {
+      path: '/api-http/auth-implementation',
+      method: 'get',
+      handler: serve_file_md('authorization-implementation.md')
+    },
 
-  server.route({
-    path: '/api-http/auth',
-    method: 'get',
-    handler: serve_file_md('authorization.md')
-  });
+    // special
+    {
+      path: '/access',
+      method: 'get',
+      handler: serve_file_md('access.md')
+    },
+    {
+      path: '/workshop',
+      method: 'get',
+      handler: serve_file_md('workshop.md')
+    },
 
-  server.route({
-    path: '/api-http/auth-implementation',
-    method: 'get',
-    handler: serve_file_md('authorization-implementation.md')
-  });
+    // redirects
+    {
+      path: '/api-rest',
+      method: 'get',
+      handler: function(request, reply) {
+        reply().redirect('/api-http')
+      }
+    },
 
+    // assets
 
-  // special
-
-  server.route({
-    path: '/access',
-    method: 'get',
-    handler: serve_file_md('access.md')
-  });
-
-  server.route({
-    path: '/workshop',
-    method: 'get',
-    handler: serve_file_md('workshop.md')
-  });
-
-
-  // redirects
-
-  server.route({
-    path: '/',
-    method: 'get',
-    handler: function(request, reply) {
-      reply().redirect('/api-http');
-    }
-  });
-
-  server.route({
-    path: '/api-rest',
-    method: 'get',
-    handler: function(request, reply) {
-      reply().redirect('/api-http');
-    }
-  });
-
-
-  // assets
-
-  server.route({
-    path: '/assets/{path*}',
-    method: 'get',
-    handler: {
-      directory: {
-        path: path.join(__dirname, '../client/')
+    {
+      path: '/assets/{path*}',
+      method: 'get',
+      handler: {
+        directory: {
+          path: path.join(__dirname, '../client/')
+        }
       }
     }
-  });
+  ])
 
-};
+}
---
+++ views/index.md
@@ -0,0 +1,25 @@
+# Welcome, littleBits Developer!
+
+
+## Resources & Libraries
+
+We do everything in javascript. We like javascript and hope you do too! Here are
+some resources to get you up and running.
+
+
+#### @littleBits javascript libraries
+
+* [HTTP API library](https://github.com/littlebits/cloud-client-api-http)
+* [Streaming API library](https://github.com/littlebits/cloud-client-api-stream)
+
+
+#### Third-party libraries & Integrations
+
+* [Python: PartlyCloudy](https://github.com/technoboy10/partly-cloudy)
+
+
+## Documentation & Examples
+
+* [Getting started with the HTTP API](https://github.com/littlebits/cloud-api-lessons)
+* [Documentation: HTTP REST API](/api-http)
+* [Documentation: Websocket Streaming API](/api-stream)
