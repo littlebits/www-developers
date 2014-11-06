1970a2bfb755c3126980ca153b8aca45ec5d3a8d
Jason Kuhrt <jasonkuhrt@me.com>
2014-11-06 00:33:15 -0500

    Initial raw route display

--- Makefile
+++ Makefile
@@ -1,12 +1,11 @@
 dev:
-	PORT=8000 NODE_ENV=dev node-dev server
-
-client:
-	duo client/index.css client/index.js build
-	node_modules/.bin/myth build/index.css build/index.css
+	PORT=8100 NODE_ENV=dev node-dev server
 
 deploy:
 	git push git@heroku.com:littlebits-cloud-developer.git master
+
+api-http-download-options:
+	http localhost:8000/options > client/api-http-routes.json
 
 logs:
 	heroku logs --tail
--- client/index.js
+++ client/index.js
@@ -1,16 +1,56 @@
 var react = require('reactjs/react-bower:react-with-addons.js')
+var routes = require('./api-http-routes.json')
+
 var E = react.DOM
+var T = react.PropTypes
+var F = react.createFactory
 
 
 
 var app = react.createClass({
-  name: 'app',
-  render: function(){
-    return E.h1(null, 'Hello World!')
+  displayName: 'app',
+  getInitialState: function(){
+    return {
+      version: '3'
+    }
+  },
+  render: function() {
+    return E.div(null, routes
+      .filter(isVersion(this.state.version))
+      .map(RouteMapper))
   }
 })
 
 
-document.addEventListener('DOMContentLoaded', function(){
-  react.render(react.createElement(app), document.getElementById('app-container'))
-})
+
+function RouteMapper(route) {
+  var key = route.method + route.version + route.path
+  return Route({ key: key, route: route })
+}
+
+var Route = F(react.createClass({
+  displayName: 'route',
+  propTypes: {
+    route: T.object.isRequired
+  },
+  render: function(){
+    return E.h1(null, this.props.route.path)
+  }
+}))
+
+
+bootstrap(app)
+
+
+function isVersion(n) {
+  return function(route){
+    return String(route.version) === n
+  }
+}
+
+function bootstrap(app, selector){
+  selector = selector || '#app-container'
+  document.addEventListener('DOMContentLoaded', function(){
+    react.render(react.createElement(app), document.querySelector(selector))
+  })
+}
