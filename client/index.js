b78cc88bbdeb730015f5f49a47cbd99640b95a9a
Jason Kuhrt <jasonkuhrt@me.com>
2014-11-05 19:33:23 -0500

    React Hello World

--- client/index.js
+++ client/index.js
@@ -1,1 +1,16 @@
-console.log('foobar')
+var react = require('reactjs/react-bower:react-with-addons.js')
+var E = react.DOM
+
+
+
+var app = react.createClass({
+  name: 'app',
+  render: function(){
+    return E.h1(null, 'Hello World!')
+  }
+})
+
+
+document.addEventListener('DOMContentLoaded', function(){
+  react.render(react.createElement(app), document.getElementById('app-container'))
+})
