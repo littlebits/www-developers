f93c1aadbeb47fb54b40a2f461a81eade01ec15c
Jason Kuhrt <jasonkuhrt@me.com>
2014-11-13 16:05:57 -0500

    Add "Fork me on Github" ribbon

--- client/parts/banner.js
+++ client/parts/banner.js
@@ -9,8 +9,22 @@ var Banner = r.createClass({
   render: function() {
     return e.
     div({ className: 'banner themeDark' },
+      githubRibbon({ href: 'https://github.com/littlebits/www-developers' }),
       e.h1({className: 'fontHeading'}, 'littleBits Cloud Platform'))
   }
 })
 
 module.exports = r.createFactory(Banner)
+
+
+var githubRibbon = r.createFactory(r.createClass({
+  displayName: 'github-ribbon',
+  render: function() {
+    return e.
+    div({ className: 'github-fork-ribbon-wrapper right'},
+      e.div({ className: 'github-fork-ribbon' },
+        e.a({ href: this.props.href }, 'Fork me on Github')
+      )
+    )
+  }
+}))
